5a12513281dfce4e66df44cb90c1e2ef1c9f9ba5
Simriti <simritib1@gmail.com>
2023-12-31 15:37:15 +0400

    Create Introduction-to-Ruby.rb

---
+++ Introduction-to-Ruby.rb
@@ -0,0 +1,29 @@
+# Data Types: Numbers, Strings, Booleans
+my_num = 25    
+my_boolean = true  
+my_string = "Ruby"  
+
+puts my_num
+puts my_boolean
+puts my_string
+
+# Variables 
+my_num = 25
+
+# Math
+=begin
+Addition (+)
+Subtraction (-)
+Multiplication (*)
+Division (/)
+Exponentiation (**)
+Modulo (%)
+=end
+
+# 'puts' and 'print'
+=begin
+The print command just takes whatever you give it and prints it to the screen. 
+puts (for “put string”) is slightly different: it adds a new (blank) line after 
+the thing you want it to print.
+=end
+
